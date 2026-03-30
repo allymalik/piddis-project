9fda3f974d866b0c67768ab1ff1d6086d1231487
allymalik <aliyanrasheed711@gmail.com>
2026-03-30 22:43:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const questionContainer = document.querySelector(".question-container");
+const resultContainer = document.querySelector(".result-container");
+const gifResult = document.querySelector(".gif-result");
+const heartLoader = document.querySelector(".cssload-main");
+const yesBtn = document.querySelector(".js-yes-btn");
+const noBtn = document.querySelector(".js-no-btn");
+
+// NO button move (mobile safe)
+noBtn.addEventListener("mouseover", () => {
+  const parent = document.querySelector(".button-container");
+
+  const maxX = parent.clientWidth - noBtn.clientWidth;
+  const maxY = parent.clientHeight + 100;
+
+  const newX = Math.random() * maxX;
+  const newY = Math.random() * maxY;
+
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
+});
+
+// YES button
+yesBtn.addEventListener("click", () => {
+  questionContainer.style.display = "none";
+  heartLoader.style.display = "block";
+
+  setTimeout(() => {
+    heartLoader.style.display = "none";
+    resultContainer.style.display = "block";
+    gifResult.play();
+  }, 2000);
+});
